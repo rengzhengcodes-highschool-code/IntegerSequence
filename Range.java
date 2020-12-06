e660ee487195148d2e5688ea252715049331aedb
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-06 15:01:34 -0500

    Converted tabs to spaces. Implemented rest of interface required functions but not tested.

--- Range.java
+++ Range.java
@@ -1,36 +1,41 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence {
-  private int start, end, current;
+	private int start, end, current;
 
-  //@param start : the starting value (inclusive) which must be <= end.
-  //@param end : the ending value which is also inclusive.
-  public Range(int start, int end) {
-    if (start <= end) {
-      this.start = start;
-      this.end = end;
-      this.current = start;
-    } else {
-      throw new IllegalArgumentException("Start " + start + " is not <= end " + end);
-    }
-  }
+	//@param start : the starting value (inclusive) which must be <= end.
+	//@param end : the ending value which is also inclusive.
+	public Range(int start, int end) {
+		if (start <= end) {
+			this.start = start;
+			this.end = end;
+			this.current = start;
+		} else {
+			throw new IllegalArgumentException("Start " + start + " is not <= end " + end);
+		}
+	}
 
-  public void reset() {
-    this.current = this.start;
-  }
-  public int length() {
-    return 0;
-  }
+	public void reset() {
+		this.current = this.start;
+	}
+	public int length() {
+		return (this.end - this.start) + 1;
+	}
 
-  //When current is no longer a valid element in the range, it should return false.
-  public boolean hasNext() {
-    return true;
-  }
+	//When current is no longer a valid element in the range, it should return false.
+	public boolean hasNext() {
+		return this.current < this.end;
+	}
 
-  //@throws NoSuchElementException when hasNext() is false.
-  //This will return the current value, it will also increase current value by 1.
-  //e.g.  if current is 5. This will make current 6, and return 5.
-  public int next() {
-    return 0;
-  }
+	//@throws NoSuchElementException when hasNext() is false.
+	//This will return the current value, it will also increase current value by 1.
+	//e.g.  if current is 5. This will make current 6, and return 5.
+	public int next() {
+		if (this.hasNext()) {
+			this.current++;
+		} else {
+			throw new NoSuchElementException("No element after this. Current: " + this.current + "\t End: ");
+		}
+		return this.current - 1;
+	}
 
 }
--- RangeTester.java
+++ RangeTester.java
@@ -1,79 +1,109 @@
 public class RangeTester {
 
-  public static void main(String[] args) {
-    boolean failure = false;
-    failure = failure || constructorTester(1000);
+	public static void main(String[] args) {
+		boolean failure = false;
+		failure = failure || constructorTester(1000);
 
-    System.out.println("\n ~~~ Overall Result ~~~");
-    if (failure) {
-      System.out.println("Coal for you!");
-    } else {
-      System.out.println("Happy Holidays!");
-    }
-  }
+		System.out.println("\n ~~~ Overall Result ~~~");
+		if (failure) {
+			System.out.println("Coal for you!");
+		} else {
+			System.out.println("Happy Holidays!");
+		}
+	}
 
-  private static void tester(String test) {
-    System.out.println("\n ~~~ " + test + "~~~\n");
-  }
+	private static void tester(String test) {
+		System.out.println("\n ~~~ " + test + "~~~\n");
+	}
 
-  private static int randInt(int end) {
-    return (int)(Math.random() * end);
-  }
+	private static int randInt(int end) {
+		return (int)(Math.random() * end);
+	}
 
-  private static int randInt(int start, int end) {
-    return (int)(Math.random()* (start - end)) + start;
-  }
+	private static int randInt(int start, int end) {
+		return (int)(Math.random()* (start - end)) + start;
+	}
 
-  private static void passMessage (int testCase) {
-    System.out.println("Test case " + testCase + " passed.");
-  }
+	private static void passMessage(int testCase) {
+		System.out.println("Test case " + testCase + " passed.");
+	}
 
-  private static void methodMessage(String method, boolean failure) {
-    if (failure) {
-      System.out.println("\nAt least one test case failed for " + method);
-    } else {
-      System.out.println(method + " PASSED");
-    }
-  }
+	private static void passMessage(String testCase) {
+		System.out.println("Test case " + testCase + " passed.");
+	}
 
-  public static boolean constructorTester(int tests) {
-    String method = "constructorTester";
-    tester(method);
-    boolean fail = false;
-    //should return true
-    for (int test = 0; test < tests; test++) {
-      int start = randInt(-100, 100);
-      int end = start + randInt(10);
-      try {
-        //tests actually works
-        Range r = new Range(start, end);
-        //tests implements IntegerSequence
-        IntegerSequence r1 = new Range(start, end);
-        //passMessage(test);
-      } catch (Exception e) {
-        e.printStackTrace();
-        fail = true;
-        System.out.println("Start: " + start);
-        System.out.println("End: " + end);
-      }
+	private static void methodMessage(String method, boolean failure) {
+		if (failure) {
+			System.out.println("\nAt least one test case failed for " + method);
+		} else {
+			System.out.println(method + " PASSED");
+		}
+	}
 
-      end = start - (1 + randInt(10));
-      try {
-        //tests actually works
-        Range r = new Range(start, end);
-        //tests implements IntegerSequence
-        IntegerSequence r1 = new Range(start, end);
-        fail = true;
-        System.out.println("Expected error!");
-        System.out.println("Start: " + start);
-        System.out.println("End: " + end);
-      } catch (Exception e) {
-        //passMessage(test);
-      }
+	public static boolean constructorTester(int tests) {
+		String method = "constructorTester";
+		tester(method);
+		boolean fail = false;
+		//should return true
+		for (int test = 0; test < tests; test++) {
+			int start = randInt(-100, 100);
+			int end = start + randInt(10);
+			try {
+				//tests actually works
+				Range r = new Range(start, end);
+				//tests implements IntegerSequence
+				IntegerSequence r1 = new Range(start, end);
+				//passMessage(test);
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail = true;
+				System.out.println("Start: " + start);
+				System.out.println("End: " + end);
+			}
 
-    }
+			end = start - (1 + randInt(10));
+			try {
+				//tests actually works
+				Range r = new Range(start, end);
+				//tests implements IntegerSequence
+				IntegerSequence r1 = new Range(start, end);
+				fail = true;
+				System.out.println("Expected error!");
+				System.out.println("Start: " + start);
+				System.out.println("End: " + end);
+			} catch (Exception e) {
+				//passMessage(test);
+			}
 
-    methodMessage(method, fail);
-    return fail;
-  }
+		}
+
+		methodMessage(method, fail);
+		return fail;
+	}
+
+	public static boolean hasNextTester(int tests) {
+		tester("lengthTester");
+		boolean fail = false;
+
+		int start = randInt(-100, 100);
+		int end = start + randInt(10);
+		Range r = new Range(start, end);
+		int[] equivalent = new int[end - start + 1];
+
+		for (int n = start; n <= end; n++) {
+			equivalent[n - start] = n;
+		}
+
+		for (int n : equivalent) {
+			if (n != equivalent[end - start /* last digit of equivalent*/]) {
+				if (r.hasNext()) {
+					passMessage(n);
+				} else {
+					System.out.println();
+				}
+			}
+		}
+
+		return fail;
+	}
 }
